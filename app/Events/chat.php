421f6ddb8106b565c7a3d5f3e17c5cbc10539295
shaimaa79 <shimo11shimo11@gmail.com>
2025-02-14 11:29:04 +0200

    first

---
+++ app/Events/chat.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class Chat implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $username;
+    public $message;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param string $username
+     * @param string $message
+     */
+    public function __construct($username, $message)
+    {
+        $this->username = $username;
+        $this->message = $message;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new Channel('chat');
+    }
+
+    
+    public function broadcastWith()
+    {
+        return [
+            'username' => $this->username,
+            'message' => $this->message,
+              
+        ];
+ 
+    }
+
+
+   
+
+}
---
+++ config/broadcasting.php
@@ -0,0 +1,63 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Broadcaster
+    |--------------------------------------------------------------------------
+    |
+    | This option controls the default broadcaster that will be used by the
+    | framework when an event needs to be broadcast. You may set this to
+    | any of the connections defined in the "connections" array below.
+    |
+    | Supported: "pusher", "redis", "log", "null"
+    |
+    */
+
+    'default' => env('BROADCAST_DRIVER', 'null'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Broadcast Connections
+    |--------------------------------------------------------------------------
+    |
+    | Here you may define all of the broadcast connections that will be used
+    | to broadcast events to other systems or over websockets. Samples of
+    | each available type of connection are provided inside this array.
+    |
+    */
+
+    'connections' => [
+
+       'pusher' => [
+            'driver' => 'pusher',
+            'key' => env('PUSHER_APP_KEY', 'local'),
+            'secret' => env('PUSHER_APP_SECRET', 'local'),
+            'app_id' => env('PUSHER_APP_ID', 'local'),
+            'options' => [
+                'cluster' => env('PUSHER_APP_CLUSTER', 'mt1'),
+                'encrypted' => false, // Set to false for local development
+                'host' => '127.0.0.1',
+                'port' => 6001,
+                'scheme' => 'http',
+                'useTLS' => false, // Set to false for local development
+            ],
+        ],
+
+        'redis' => [
+            'driver' => 'redis',
+            'connection' => 'default',
+        ],
+
+        'log' => [
+            'driver' => 'log',
+        ],
+
+        'null' => [
+            'driver' => 'null',
+        ],
+
+    ],
+
+];
